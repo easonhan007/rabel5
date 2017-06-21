5d94e6d3a012b460ac43cf55327ca53fffe8389f
easonhan <nbkhic@qq.com>
2017-06-21 14:50:11 +0800

    fix issue

--- config/puma.rb
+++ config/puma.rb
@@ -68,8 +68,8 @@ if ENV.fetch("RAILS_ENV") == 'production'
   directory "#{application_path}/current"
 
   #下面都是 puma的配置项
-  pidfile "#{application_path}/shared/tmp/pids/puma.pid"
-  state_path "#{application_path}/shared/tmp/sockets/puma.state"
+  pidfile "#{application_path}/shared/tmp/pids/#{app_name}.pid"
+  state_path "#{application_path}/shared/tmp/sockets/#{app_name}.state"
   stdout_redirect "#{application_path}/shared/log/puma.stdout.log", "#{application_path}/shared/log/puma.stderr.log"
   bind "unix://#{application_path}/shared/tmp/sockets/#{app_name}.sock"
   activate_control_app "unix://#{application_path}/shared/tmp/sockets/pumactl.sock"
